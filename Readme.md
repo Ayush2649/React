ddc0f98a6fecd3082e82dd008b35d015af3538f8
Ayush Sahu <ayushsahu1430@gmail.com>
2025-08-03 18:05:26 +0530

    Episode 2 - Parcel (The Bundler) 💪

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Namaste React 🚀
+
+# Parcel 📦
+- Dev Build
+- Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - Written in C++
+- Caching - Faster Builds
+- Image Optimization (Usually is the most costly task)
+- Minification of files
+- Bundles all the files
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - Support older browsers
+- Diagnositcs
+- Error Handling
+- Tree Shaking - Remove unused code
+- Differnet dev and prod bundles
--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  [React.createElement("div", { id: "child" }, [
+  [React.createElement("div", { id: "child", key: "child1" }, 
+    React.createElement("h1", {}, "Hi, I'm Ayush"),
+    React.createElement("h2", {}, "Hi, I'm an h2 tag"),
+  ), 
+    React.createElement("div", { id: "child2", key: "child2" }, 
     React.createElement("h1", {}, "Hi, I'm an h1 tag"),
     React.createElement("h2", {}, "Hi, I'm an h2 tag"),
-  ]), 
-    React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Hi, I'm an h1 tag"),
-    React.createElement("h2", {}, "Hi, I'm an h2 tag"),
-  ])]
+  )]
 );
 
 console.log(parent);
