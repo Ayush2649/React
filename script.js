445187aad2ce1d9289ab71ed921a1e5bb283653a
Ayush Sahu <ayushsahu1430@gmail.com>
2025-08-08 21:02:11 +0530

    Episode 3 - React Components

--- script.js
+++ script.js
@@ -6,13 +6,23 @@ import ReactDOM from "react-dom/client";
 // JSX is different, React is different
 // JSX is not HTML inside JS, It may be HTML like syntax but it is not HTML
 
-const JSXHeading = (
+const Title = () => (
   <h1 className="root" tabIndex={1}>
     Namaste React using JSX 🚀
   </h1>
 );
 
+// React Components - Class Based Components (OLD) and Functional Components (NEW)
+
+// React Functional Component - A normal function which returns JSX
+// Component Composition - We can create a component and use it inside another component
+const HeadingComponent = () => (
+  <div id="container">
+  <Title />
+    <h1>Namaste React Functional Component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(JSXHeading);
-// console.log(JSXHeading);
+root.render(<HeadingComponent />);
