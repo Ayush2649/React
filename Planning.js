82704b80b38c0eda1d34d3e3559a7b6a01c23b8d
Ayush Sahu <ayushsahu1430@gmail.com>
2025-08-16 13:13:28 +0530

    Done with the restaurant cards 🍽️

---
+++ Planning.js
@@ -0,0 +1,21 @@
+/*
+*Header
+    * Logo
+    * Nav Items
+    * Search Bar
+*Body
+    * Restaurant Cards
+        * Image
+        * Name
+        * Cuisine
+        * Rating
+        * ETA (Expected Time of Arrival)
+*Footer
+    * Copyright
+    * Contact Info
+    * Terms of Service
+*/
+
+// Functional Component is nothing but a JavaScript function that returns a React element.
+// Props (Properties) are nothing but arguements to that function
+// Passing a prop to a component is just like passing an arguement to a function
--- script.js
+++ script.js
@@ -1,28 +1,66 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => object => render (html element)
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://images-platform.99static.com/O3ZHfJeHB741xpyYH95tWvMsdTI=/0x0:1279x1279/500x500/top/smart/99designs-contests-attachments/63/63966/attachment_63966256"
+          alt="Logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Search</li>
+          <li>Offers</li>
+          <li>Help</li>
+          <li>Sign In</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// JSX is different, React is different
-// JSX is not HTML inside JS, It may be HTML like syntax but it is not HTML
+const RestaurantCard = (props) => {
+  const { resName, cuisine, source } = props;
+  
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f9ffe2" }}>
+      <img
+        src={source}
+        alt="Res-logo"
+      />
+      <h3>{resName}</h3>
+      <h4>{cuisine}</h4>
+      <p>4.5 stars</p>
+      <p>30 mins</p>
+    </div>
+  );
+};
 
-const Title = () => (
-  <h1 className="root" tabIndex={1}>
-    Namaste React using JSX 🚀
-  </h1>
-);
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="res-container">
+        <RestaurantCard resName="Hotel Sainath" cuisine="North Indian, Chienese" source="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/vkhcohhmqfczycw9vsar"/>
+        <RestaurantCard resName="Satkar Restaurant" cuisine="South Indian, Salads" source="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rvxp5xbniat84r6efku2"/>
+      </div>
+    </div>
+  );
+};
 
-// React Components - Class Based Components (OLD) and Functional Components (NEW)
-
-// React Functional Component - A normal function which returns JSX
-// Component Composition - We can create a component and use it inside another component
-const HeadingComponent = () => (
-  <div id="container">
-  <Title />
-    <h1>Namaste React Functional Component</h1>
-  </div>
-);
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
