31a31d2abbed87e8ba5dfdc748f30145d17389ac
Ayush Sahu <ayushsahu1430@gmail.com>
2025-08-16 20:56:23 +0530

    Done with the Dynamic Data on Cards .🧨

--- Planning.js
+++ Planning.js
@@ -19,3 +19,4 @@
 // Functional Component is nothing but a JavaScript function that returns a React element.
 // Props (Properties) are nothing but arguements to that function
 // Passing a prop to a component is just like passing an arguement to a function
+// Config-driven UI, also known as Configuration-Driven UI (CDUI), is a design pattern where the structure, layout, and behavior of a user interface are defined and controlled by external configuration data rather than being hard-coded within the application's source code. 
