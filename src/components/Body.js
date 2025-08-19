9a275c9f29a3ad42d28fd9f16796f856719f59d6
Ayush Sahu <ayushsahu1430@gmail.com>
2025-08-19 10:14:36 +0530

    Done with Dynamic Layout

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,50 @@
 import RestaurantCard from "./RestaurantCard";
-import { resList } from "../utils/demoData";
 
 const Body = () => {
+  let listOfRestaurants = [
+    {
+      info: {
+        id: "234875",
+        name: "Adil Hotel",
+        cloudinaryImageId: "gp1ityra6utvzqn6ghnv",
+        costForTwo: "₹150 for two",
+        cuisines: ["Biryani", "Tandoor"],
+        avgRating: 4.4,
+        sla: {
+          deliveryTime: 28,
+        },
+      },
+    },
+    {
+      info: {
+        id: "234876",
+        name: "Adi Hotel",
+        cloudinaryImageId: "gp1ityra6utvzqn6ghnv",
+        costForTwo: "₹150 for two",
+        cuisines: ["Biryani", "Tandoor"],
+        avgRating: 3.4,
+        sla: {
+          deliveryTime: 28,
+        },
+      },
+    },
+  ];
+
   return (
     <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            listOfRestaurants = listOfRestaurants.filter((res) => res.info.avgRating > 4);
+            console.log(listOfRestaurants);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard resData={restaurant} key={restaurant.info.id} />
         ))}
       </div>
